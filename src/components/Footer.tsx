e8da141aac875a57280389462d0575fefc0659eb
Matheus Figueiredo <matheus.figueiredo@br.ey.com>
2025-07-11 17:11:12 -0300

    modificacoes do cursor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t p-4 mt-8 text-center text-sm">
+    <footer className="border-t p-4 mt-40 text-center text-sm">
       <p>&copy; {new Date().getFullYear()} CEAD. Todos os direitos reservados.</p>
     </footer>
   );
